35e60a2e1f709e7c57204a331f6560989cfc54c7
deepakchauhan33 <deep651965@gmail.com>
2026-01-25 16:15:15 +0530

    Create time and date functionalty

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const addBtn = document.querySelector('.addBtn');
+const addBtn = document.querySelector(".addBtn");
 
-addBtn.addEventListener('click', ()=>{
-    console.log("Clicked");
-})
+let dayVal = document.querySelector(".dayVal");
+// dayVal.classList.add('text-[28px], font-bold')
+
+let timeVal = document.querySelector(".timeVal");
+// timeVal.classList.add('text-[28px], font-bold')
+
+const timeDate = () => {
+  let myDate = new Date();
+  let customDate = myDate.toLocaleDateString("en-US", {
+    weekday: "long",
+    day: "2-digit",
+    month: "long",
+  });
+
+  const hours = String(myDate.getHours()).padStart(2, "0");
+  const minutes = String(myDate.getMinutes()).padStart(2, "0");
+
+  dayVal.innerText = customDate;
+timeVal.innerText = `${hours}:${minutes}`;
+
+};
+
+setInterval(timeDate, 100);
+
+
