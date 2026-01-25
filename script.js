75c829cb319e0d8371ddb092d90b7a21682b25bc
deepakchauhan33 <deep651965@gmail.com>
2026-01-25 21:33:06 +0530

    Create add task to queue functionality

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const addBtn = document.querySelector(".addBtn");
-
 let dayVal = document.querySelector(".dayVal");
-// dayVal.classList.add('text-[28px], font-bold')
-
 let timeVal = document.querySelector(".timeVal");
-// timeVal.classList.add('text-[28px], font-bold')
+const inputTask = document.querySelector('.inputTask');
+const addBtn = document.querySelector('.addBtn');
+const taskQueue = document.querySelector('.taskQueue');
 
+
+
+
+// Shows current time and date 
 const timeDate = () => {
   let myDate = new Date();
   let customDate = myDate.toLocaleDateString("en-US", {
@@ -18,10 +20,36 @@ const timeDate = () => {
   const minutes = String(myDate.getMinutes()).padStart(2, "0");
 
   dayVal.innerText = customDate;
-timeVal.innerText = `${hours}:${minutes}`;
+  timeVal.innerText = `${hours}:${minutes}`;
 
 };
 
+
+addBtn.addEventListener('click', () => {
+  console.log('Click')
+
+  if (inputTask.value.trim() === "") {
+    alert('Please Enter some task!!!')
+    inputTask.value = "";
+  } else {
+    addTask(inputTask.value.trim());
+    inputTask.value = "";
+  }
+
+})
+
+
+const addTask = (val) => {
+  console.log("clicked");
+  const task = document.createElement('li');
+  task.classList.add('border-1','p-2')
+  task.innerText = val;
+  taskQueue.appendChild(task);
+}
+
+
+
+// Calling timeDate function at every mileseconds
 setInterval(timeDate, 100);
 
 
